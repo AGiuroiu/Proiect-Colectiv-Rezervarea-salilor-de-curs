d50777ca2de9d8305598e5d24d0bfe3c66fb841f
AGiuroiu <agiurou@gmail.com>
2024-04-17 17:52:38 +0300

    mica reordonare

--- pc/src/App.js
+++ pc/src/App.js
@@ -1,80 +1,18 @@
-import './App.css'; 
-import React, { useEffect } from 'react';
-import Logo from './images/roomranger.png'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Asigură-te că importul este corect
 
-function Navbar() {
+import LogIn from './components/LogIn';
+
+const App = () => {
   return (
-    <div className="navbar">
-      <div className="logo">
-        <div className="logo-container">
-                        <img src={Logo} alt="Logo" className="logo" />
-                    </div>
-                    </div>
-      <div className="nav-items">
-        <a href="/">Home</a>
-        <a href="/map">Map</a>
-        <a href="/about-us">About Us</a>
-        <a href="/profile">Profile</a>
-      </div>
-    </div>
+    <Router>
+      <Routes> {/* Schimbă acest lucru */}
+        <Route path="/" element={<LogIn />} /> {/* Schimbă acest lucru */}
+      </Routes> {/* Schimbă acest lucru */}
+    </Router>
   );
-}
+};
 
-function Map() {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyD3pcEWmYaPqKLCEGeu8fuZ5fFvSVvWeC8&libraries=places`;
-    script.async = true;
-    script.defer = true;
-    script.onload = initializeMap;
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
-  function initializeMap() {
-    const map = new window.google.maps.Map(document.getElementById('map'), {
-      center: { lat: 45.763361655442004,  lng: 21.220691404711896 },
-      zoom: 14
-    
-    });
-
-    const marker = new window.google.maps.Marker({
-      position: { lat: -34.397, lng: 150.644 },
-      map: map,
-      title: 'Marker'
-    });
-  }
-
-  return (
-    <div id="map" className="map" style={{ width: '70%', height: '580px'}}></div>
-  );
-}
-
-export { Map };
-
-// Componenta pentru butoanele de la baza paginii
-function ActionButtons() {
-  return (
-    <div className="action-buttons">
-      <button className="search-btn">Search</button>
-      <button className="filter-btn">Filter</button>
-      <button className="book-btn">Book</button>
-    </div>
-  );
-}
-
-// Componenta principală App care leagă totul împreună
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Map />
-      <ActionButtons />
-    </div>
-  );
-}
-
-export default App;
+ReactDOM.render(<App />, document.getElementById('root'));
+export default LogIn.jsx;
--- pc/src/components/map.jsx
+++ pc/src/components/map.jsx
@@ -1,11 +1,29 @@
+import './style/Map.css'; 
 import React, { useEffect } from 'react';
-import './style/Main.css'
+import Logo from './images/roomranger.png'
 
-export function Map() {
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <div className="logo-container">
+                        <img src={Logo} alt="Logo" className="logo" />
+                    </div>
+                    </div>
+      <div className="nav-items">
+        <a href="/">Home</a>
+        <a href="/map">Map</a>
+        <a href="/about-us">About Us</a>
+        <a href="/profile">Profile</a>
+      </div>
+    </div>
+  );
+}
+
+function Map() {
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyD3pcEWmYaPqKLCEGeu8fuZ5fFvSVvWeC8
-    &libraries=places`;
+    script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyD3pcEWmYaPqKLCEGeu8fuZ5fFvSVvWeC8&libraries=places`;
     script.async = true;
     script.defer = true;
     script.onload = initializeMap;
@@ -18,18 +36,45 @@ export function Map() {
 
   function initializeMap() {
     const map = new window.google.maps.Map(document.getElementById('map'), {
-      center: { lat: 45.75643334548602,  lng: 21.228483079011387 },
-      zoom: 12
+      center: { lat: 45.763361655442004,  lng: 21.220691404711896 },
+      zoom: 14
+    
     });
 
     const marker = new window.google.maps.Marker({
-      position: { lat: 45.75643334548602, lng: 21.228483079011387 },
+      position: { lat: -34.397, lng: 150.644 },
       map: map,
       title: 'Marker'
     });
   }
 
   return (
-    <div id="map" className="map" style={{ width: '100%', height: '400px',  }}></div>
+    <div id="map" className="map" style={{ width: '70%', height: '580px'}}></div>
   );
 }
+
+export { Map };
+
+// Componenta pentru butoanele de la baza paginii
+function ActionButtons() {
+  return (
+    <div className="action-buttons">
+      <button className="search-btn">Search</button>
+      <button className="filter-btn">Filter</button>
+      <button className="book-btn">Book</button>
+    </div>
+  );
+}
+
+// Componenta principală App care leagă totul împreună
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Map />
+      <ActionButtons />
+    </div>
+  );
+}
+
+export default App;
