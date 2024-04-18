de4c01974cf26faac8d34a1017e5bdae318d176c
AGiuroiu <agiurou@gmail.com>
2024-04-18 18:54:29 +0300

    delete node_modules

--- pc/src/App.js
+++ pc/src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Asigură-te că importul este corect
 
-import LogIn from './components/LogIn';
+import { Login, Register } from './components';
 
 const App = () => {
   return (
     <Router>
-      <Routes> {/* Schimbă acest lucru */}
-        <Route path="/" element={<LogIn />} /> {/* Schimbă acest lucru */}
-      </Routes> {/* Schimbă acest lucru */}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
     </Router>
   );
 };
